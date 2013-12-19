178bebb207debc44c4311ca1d5f46d1e79962fc0
Patrick Adams <padams@PatrickAdam-3409.local>
2013-12-19 15:22:38 -0500

    Added random team button which selects a random team - 75% chance Survivor Team, 25% chance Infected team.

--- ZomBeacon/MainViewController.m
+++ ZomBeacon/MainViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MainViewController.h"
+#import "InfectedViewController.h"
+#import "SurvivorViewController.h"
 
 @interface MainViewController ()
 
@@ -17,6 +19,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
+
+- (IBAction)selectRandomTeam
+{
+    int randomNumber = [self getRandomNumberBetween:1 to:100];
+    
+    NSLog(@"%d", randomNumber);
+    
+    UIStoryboard *storyboard;
+    if(IS_IPAD)
+    {
+        storyboard = [UIStoryboard storyboardWithName:@"Main_iPad" bundle:nil];
+    }
+    else
+    {
+        storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    }
+    
+    if (randomNumber > 0 && randomNumber < 75 ) {
+        InfectedViewController *vc = [storyboard instantiateViewControllerWithIdentifier:@"infected"];
+        [self presentModalViewController:vc animated:YES];
+    } else {
+        SurvivorViewController *vc = [storyboard instantiateViewControllerWithIdentifier:@"survivor"];
+        [self presentModalViewController:vc animated:YES];
+    }
+}
+
+-(int)getRandomNumberBetween:(int)from to:(int)to {
+    
+    return (int)from + arc4random() % (to-from+1);
 }
 
 - (void)didReceiveMemoryWarning
