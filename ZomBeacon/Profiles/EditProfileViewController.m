a60cddd8d8e6b810032a4e8278b2a1f8614f4db0
Patrick <patrickleeadams@gmail.com>
2014-02-20 10:40:29 -0500

    Profile folder was not being added to the git repo

---
+++ ZomBeacon/Profiles/EditProfileViewController.m
@@ -0,0 +1,49 @@
+//
+//  EditProfileViewController.m
+//  ZomBeacon
+//
+//  Created by Patrick Adams on 2/5/14.
+//  Copyright (c) 2014 Patrick Adams. All rights reserved.
+//
+
+#import "EditProfileViewController.h"
+
+@interface EditProfileViewController ()
+
+@end
+
+@implementation EditProfileViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	currentUser = [PFUser currentUser];
+    self.nameField.text = currentUser[@"name"];
+    self.usernameField.text = currentUser.username;
+    self.emailField.text = currentUser.email;
+    self.bioField.text = currentUser[@"bio"];
+}
+
+- (IBAction)saveProfileChanges
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    currentUser[@"name"] = self.nameField.text;
+    currentUser.username = self.usernameField.text;
+    currentUser.email = self.emailField.text;
+    currentUser[@"bio"] = self.bioField.text;
+    
+    [currentUser saveInBackground];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Changes Saved!" message:@"The changes you've made have been saved." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    [alert show];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
