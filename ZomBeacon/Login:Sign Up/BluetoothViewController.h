38d5bb4aad233dc50bac695dd8a387a7f3f991e9
Patrick <patrickleeadams@gmail.com>
2014-02-28 23:35:06 -0500

    Adjustments

---
+++ ZomBeacon/Login:Sign Up/BluetoothViewController.h
@@ -0,0 +1,16 @@
+//
+//  BluetoothViewController.h
+//  ZomBeacon
+//
+//  Created by Patrick Adams on 2/28/14.
+//  Copyright (c) 2014 Patrick Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BluetoothViewController : UIViewController
+
+@property (nonatomic, strong) IBOutletCollection (UILabel)NSArray *titilliumSemiBoldFonts;
+@property (nonatomic, strong) IBOutletCollection (UILabel)NSArray *titilliumRegularFonts;
+
+@end
